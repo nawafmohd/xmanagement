9e7219f22a99c55f7e18ed5548459726354a8af3
nawafmohd <nawafaljumaiah@gmail.com>
2023-07-10 07:05:29 +0300

    second commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	
+	"os"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
 }
